e1119647f839fa00d7d502321148fcaaad9920fa
rauf22 <rafik2516@gmail.com>
2020-04-27 14:25:37 +0300

    keyframes square

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [toggle, setToggle] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      <hr/>
+      <div className="blocks">
+        { toggle && <div className="square blue">{toggle.toString()}</div>}
+      </div>
+   </div>
+  )
 }
 
-export default App;
+export default App
